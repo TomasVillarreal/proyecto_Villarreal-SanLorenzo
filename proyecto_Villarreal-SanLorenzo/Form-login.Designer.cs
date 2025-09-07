ee3e0a0108918b2a7431332e5145c1de492f54ac
juancruz-sanlorenzo <sanlorenzojuancruz@gmail.com>
2025-09-07 13:37:38 -0300

    Primer commit sobre esta rama nueva. Formulario del login. Esto es mas un regsitro

---
+++ proyecto_Villarreal-SanLorenzo/Form-login.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace proyecto_Villarreal_SanLorenzo
+{
+    partial class Form_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_login));
+            labelClinicks = new Label();
+            labelBienvenido = new Label();
+            labelA = new Label();
+            labelSH = new Label();
+            tbUsuario = new TextBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            pictureBox1 = new PictureBox();
+            labelUsuario = new Label();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            pictureBox2 = new PictureBox();
+            labelPass = new Label();
+            tbPass = new TextBox();
+            bIniciarSesion = new Button();
+            bOjo = new Button();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // labelClinicks
+            // 
+            labelClinicks.AutoSize = true;
+            labelClinicks.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelClinicks.Location = new Point(301, 141);
+            labelClinicks.Name = "labelClinicks";
+            labelClinicks.Size = new Size(95, 32);
+            labelClinicks.TabIndex = 2;
+            labelClinicks.Text = "Clinicks";
+            labelClinicks.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelBienvenido
+            // 
+            labelBienvenido.AutoSize = true;
+            labelBienvenido.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelBienvenido.Location = new Point(289, 46);
+            labelBienvenido.Name = "labelBienvenido";
+            labelBienvenido.Size = new Size(135, 32);
+            labelBienvenido.TabIndex = 3;
+            labelBienvenido.Text = "Bienvenido";
+            labelBienvenido.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelA
+            // 
+            labelA.AutoSize = true;
+            labelA.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelA.Location = new Point(335, 94);
+            labelA.Name = "labelA";
+            labelA.Size = new Size(30, 32);
+            labelA.TabIndex = 4;
+            labelA.Text = "A";
+            labelA.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelSH
+            // 
+            labelSH.AccessibleRole = AccessibleRole.None;
+            labelSH.AutoSize = true;
+            labelSH.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelSH.Location = new Point(266, 202);
+            labelSH.Name = "labelSH";
+            labelSH.Size = new Size(188, 25);
+            labelSH.TabIndex = 6;
+            labelSH.Text = "Sistema Hospitalario";
+            labelSH.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tbUsuario
+            // 
+            tbUsuario.BackColor = Color.WhiteSmoke;
+            tbUsuario.BorderStyle = BorderStyle.FixedSingle;
+            tbUsuario.Cursor = Cursors.Hand;
+            tbUsuario.ForeColor = Color.Black;
+            tbUsuario.Location = new Point(358, 256);
+            tbUsuario.Name = "tbUsuario";
+            tbUsuario.Size = new Size(189, 27);
+            tbUsuario.TabIndex = 7;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.Controls.Add(pictureBox1);
+            flowLayoutPanel1.Controls.Add(labelUsuario);
+            flowLayoutPanel1.Location = new Point(180, 257);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(156, 31);
+            flowLayoutPanel1.TabIndex = 9;
+            flowLayoutPanel1.WrapContents = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(31, 23);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // labelUsuario
+            // 
+            labelUsuario.Anchor = AnchorStyles.None;
+            labelUsuario.AutoSize = true;
+            labelUsuario.Font = new Font("Verdana", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelUsuario.Location = new Point(40, 4);
+            labelUsuario.Name = "labelUsuario";
+            labelUsuario.Size = new Size(82, 20);
+            labelUsuario.TabIndex = 0;
+            labelUsuario.Text = "Usuario";
+            labelUsuario.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.AutoSize = true;
+            flowLayoutPanel2.Controls.Add(pictureBox2);
+            flowLayoutPanel2.Controls.Add(labelPass);
+            flowLayoutPanel2.Location = new Point(180, 306);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(159, 41);
+            flowLayoutPanel2.TabIndex = 10;
+            flowLayoutPanel2.WrapContents = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(3, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(31, 35);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // labelPass
+            // 
+            labelPass.Anchor = AnchorStyles.None;
+            labelPass.AutoSize = true;
+            labelPass.Font = new Font("Verdana", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelPass.Location = new Point(40, 10);
+            labelPass.Name = "labelPass";
+            labelPass.Size = new Size(116, 20);
+            labelPass.TabIndex = 0;
+            labelPass.Text = "Contraseña";
+            labelPass.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // tbPass
+            // 
+            tbPass.BackColor = Color.WhiteSmoke;
+            tbPass.BorderStyle = BorderStyle.FixedSingle;
+            tbPass.Cursor = Cursors.Hand;
+            tbPass.ForeColor = Color.Black;
+            tbPass.Location = new Point(358, 317);
+            tbPass.Name = "tbPass";
+            tbPass.PasswordChar = '*';
+            tbPass.Size = new Size(189, 27);
+            tbPass.TabIndex = 11;
+            // 
+            // bIniciarSesion
+            // 
+            bIniciarSesion.Cursor = Cursors.Hand;
+            bIniciarSesion.FlatAppearance.BorderColor = Color.Silver;
+            bIniciarSesion.FlatAppearance.MouseDownBackColor = Color.LightGray;
+            bIniciarSesion.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            bIniciarSesion.FlatStyle = FlatStyle.Flat;
+            bIniciarSesion.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bIniciarSesion.ForeColor = Color.Black;
+            bIniciarSesion.Location = new Point(301, 390);
+            bIniciarSesion.Name = "bIniciarSesion";
+            bIniciarSesion.Size = new Size(135, 29);
+            bIniciarSesion.TabIndex = 12;
+            bIniciarSesion.Text = "Iniciar Sesión";
+            bIniciarSesion.UseVisualStyleBackColor = true;
+            bIniciarSesion.Click += bIniciarSesion_Click;
+            // 
+            // bOjo
+            // 
+            bOjo.Cursor = Cursors.Hand;
+            bOjo.FlatAppearance.BorderColor = Color.Silver;
+            bOjo.FlatAppearance.MouseDownBackColor = Color.LightGray;
+            bOjo.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            bOjo.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bOjo.ForeColor = Color.Black;
+            bOjo.Image = (Image)resources.GetObject("bOjo.Image");
+            bOjo.Location = new Point(553, 317);
+            bOjo.Name = "bOjo";
+            bOjo.Size = new Size(33, 29);
+            bOjo.TabIndex = 13;
+            bOjo.UseVisualStyleBackColor = true;
+            bOjo.Click += bOjo_Click;
+            // 
+            // Form_login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 512);
+            Controls.Add(bOjo);
+            Controls.Add(bIniciarSesion);
+            Controls.Add(tbPass);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(tbUsuario);
+            Controls.Add(flowLayoutPanel2);
+            Controls.Add(labelSH);
+            Controls.Add(labelA);
+            Controls.Add(labelBienvenido);
+            Controls.Add(labelClinicks);
+            Name = "Form_login";
+            Text = "Form_login";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            flowLayoutPanel2.ResumeLayout(false);
+            flowLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelClinicks;
+        private Label labelBienvenido;
+        private Label labelA;
+        private Label labelSH;
+        private TextBox tbUsuario;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private PictureBox pictureBox1;
+        private Label labelUsuario;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private PictureBox pictureBox2;
+        private Label labelPass;
+        private TextBox tbPass;
+        private Button bIniciarSesion;
+        private Button bOjo;
+    }
+}
---
+++ proyecto_Villarreal-SanLorenzo/Form-login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_Villarreal_SanLorenzo
+{
+    public partial class Form_login : Form
+    {
+        private bool mostrando = false; //Utilizado para el btn que muestra la oculta el password.
+
+        public Form_login()
+        {
+            InitializeComponent();
+        }
+
+        private void bIniciarSesion_Click(object sender, EventArgs e)
+        {
+            string usuario = tbUsuario.Text.Trim();
+            string password = tbPass.Text.Trim();
+
+            if (usuario == "admin" && password == "1234")
+            {
+                Form1 formHome = new Form1();
+                formHome.Show();
+
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña incorrectos!",
+                    "Error inicio de sesión", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void bOjo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
